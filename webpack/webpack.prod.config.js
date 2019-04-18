28b2d818e0e95819ab01c9650d1e6157dd87c140
Caleb Zimmerman <czimmerman@c02yc0hajgh7.ci.org>
2019-04-18 12:19:57 -0600

    dd clean-webpack-plugin

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const productionConfig = {
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
 
 module.exports = merge.smart(baseConfig, productionConfig)
