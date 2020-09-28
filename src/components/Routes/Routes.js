e4a271d50c5c513a9cd56fe61d61adb4128f44df
Steven Smith <StSmith@us.ci.org>
2020-09-28 13:36:24 -0700

    Remove some default configs

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 
 import Home from '@pages/Home'
+import Register from '@pages/Register'
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={Home} />
+    <Route exact path='/register' component={Register} />
   </Switch>
 )
 
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './Register.module.css'
+
+const Register = () => {
+  return <p className={styles.home}>React Template</p>
+}
+
+export default Register
