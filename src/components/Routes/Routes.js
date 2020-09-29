6ffbd78cb34910365a7b684f1d50cea52414f66b
jessicaskl <jessicalee108@gmail.com>
2020-09-29 11:50:16 -0400

    redirecting

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router'
 
 import Home from '@pages/Home'
 import Register from '@pages/Register'
+import Beneficiaries from '../../pages/Beneficiaries'
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={Home} />
     <Route exact path='/register' component={Register} />
+    <Route exact path='/beneficiaries' component={Beneficiaries} />
   </Switch>
 )
 
---
+++ src/pages/Beneficiaries/Beneficiaries.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './Beneficiaries.module.css'
+
+const Beneficiaries = () => {
+  return <div className={styles.beneficiaries}>Beneficiaries</div>
+}
+
+export default Beneficiaries
