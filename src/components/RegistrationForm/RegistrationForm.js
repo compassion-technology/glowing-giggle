0cb4536b075db3ef93b256fa954f8d771784b942
jessicaskl <jessicalee108@gmail.com>
2020-09-29 11:50:16 -0400

    redirecting

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FormInput, useForm, EVENT_TYPES, VALIDATION_DISPLAY } from '@compassion-technology/forms'
 import { isEmail, isNotEmpty, isNumeric } from '@compassion-technology/validators'
 
@@ -6,9 +6,13 @@ import { getLanguageValue } from '@utils/localization'
 
 import styles from './RegistrationForm.module.css'
 import { mockValidateNoEligibleChildren, mockValidateOK } from '../../utils/mock/validate'
+import Beneficiaries from '../../pages/Beneficiaries'
+import { Redirect } from 'react-router-dom'
 
 const RegistrationForm = () => {
   const [error, setError] = useState('')
+  const [shouldRedirect, setShouldRedirect] = useState(false)
+
   const { Form, values, isSubmitting, setIsSubmitting } = useForm()
   const { email, id } = values
 
@@ -20,6 +24,7 @@ const RegistrationForm = () => {
       if (email.includes('john')) {
         const { data } = await mockValidateOK()
 
+        setShouldRedirect(true)
         console.log(data)
       } else {
         const { errors } = await mockValidateNoEligibleChildren()
@@ -33,7 +38,9 @@ const RegistrationForm = () => {
 
   const isValid = email || id > 0
 
-  return (
+  return shouldRedirect ? (
+    <Redirect from='/' to={`beneficiaries/${id}`} noThrow />
+  ) : (
     <div className={styles.register}>
       <Form onSubmit={onSubmit} validateOnEvents={[EVENT_TYPES.onBlur]}>
         <FormInput
@@ -61,7 +68,12 @@ const RegistrationForm = () => {
         <div className={styles.error}>{error}</div>
 
         <div className={styles.actions}>
-          <button type='submit' disabled={!isValid} data-submitting={isSubmitting}>
+          <button
+            type='submit'
+            disabled={!isValid}
+            data-submitting={isSubmitting}
+            onSubmit={onSubmit}
+          >
             {getLanguageValue('Next')}
           </button>
         </div>
