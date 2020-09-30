d4ed104594ce2dd796f896c53fc79a041fe70758
Chloe <ccummings@us.ci.org>
2020-09-30 11:40:33 -0600

    adding mock response for beneficiary submitting microsite request

--- src/utils/mock/submit.js
+++ src/utils/mock/submit.js
@@ -1,5 +1,12 @@
 import { sleep } from './common'
 
+const submitBeneficiary = {
+  email: 'supporter@us.ci.org',
+  address: 'Compassion International \nColorado Springs, CO 80997',
+  skype: 'compassion',
+  facebook: 'compassionintl'
+}
+
 export const mockSubmitOK = async () => {
   await sleep()
   return {}
@@ -8,4 +15,10 @@ export const mockSubmitOK = async () => {
 export const mockSubmitError = async () => {
   await sleep()
   return {}
+}
+
+// if a beneficiary is registering through microsite, return supporter's contact information
+export const mockSubmitBeneficiary = async () => {
+  await sleep()
+  return submitBeneficiary
 }
