b3da2faadc1690548c4d1de09749d5db99021f17
Chloe <ccummings@us.ci.org>
2020-09-30 11:53:02 -0600

    add case for multiple chats

--- src/utils/mock/chat.js
+++ src/utils/mock/chat.js
@@ -1,7 +1,12 @@
-import { sleep } from './common'
+import { beneficiaryList, sleep } from './common'
 
-//when registering for chat app, redirect to chat window for that supporter/beneficiary pair
+//when logging in/registering for chat app, redirect to chat window for that supporter/beneficiary pair
 export const registerOK = async () => {
   await sleep()
   return '/chat/123/12345678'
+}
+
+//when supporter is logging in and has multiple participating beneficiaries, select which chat
+export const registerMultipleBeneficiaries = async () => {
+  return beneficiaryList
 }
